4314de1e2b7cd90cf66ea8da8bbc87c2779a61ea
danielflorencio <danielflorencio20@gmail.com>
2023-01-27 11:47:12 -0300

    updating repository

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import CssBaseline from '@mui/material/CssBaseline';
+// import Button from '@mui/material/Button';
+import SignIn from './assets/pages/SignInPage/SignInPage';
+export default function App() {
+  return (
+    <>
+      <CssBaseline enableColorScheme/>
+      <SignIn/>
+    </>
+  )
+}
